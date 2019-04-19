a64b92e9f952f818271fede3c72e7b2dfdb48b2a
wubian.wb <wubian.wb@alibaba-inc.com>
2019-04-19 14:32:03 +0800

    160 相交链表

--- src/main/java/cn/bw/leetcode/A160相交链表.java
+++ src/main/java/cn/bw/leetcode/A160相交链表.java
@@ -1,4 +1,4 @@
-package cn.bw.dsa.leecode;
+package cn.bw.leetcode;
 
 /**
  * @description:
@@ -32,8 +32,7 @@ public class A160相交链表 {
          **/
         if (headA == null || headB == null) return null;
         ListNode pA = headA, pB = headB;
-        // 在这里第一轮体现在pA和pB第一次到达尾部会移向另一链表的表头,
-        // 而第二轮体现在如果pA或pB相交就返回交点, 不相交最后就是null==null
+
         while (pA != pB) {
             pA = pA == null ? headB : pA.next;
             pB = pB == null ? headA : pB.next;
