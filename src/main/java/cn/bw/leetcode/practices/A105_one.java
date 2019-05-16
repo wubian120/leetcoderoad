3f44dd3737ae99f18bed2c6f70e7ee8ef5231da6
WuBian <bian.wu@ele.me>
2019-05-16 14:55:18 +0800

    11

---
+++ src/main/java/cn/bw/leetcode/practices/A105_one.java
@@ -0,0 +1,28 @@
+package cn.bw.leetcode.practices;
+
+public class A105_one {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+
+    }
+
+    private int searchInorderIndex(int[] inorder, int start, int end, int data){
+
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int left, int right){
+
+    }
+
+}
