4e8892413032dd46db3ae6a815bc3bc518ba04a9
Richard.Wu <120793495@qq.com>
2019-05-24 11:12:55 +0800

    145 后序遍历 递归解法

--- src/main/java/cn/bw/leetcode/A145二叉树的后序遍历.java
+++ src/main/java/cn/bw/leetcode/A145二叉树的后序遍历.java
@@ -1,11 +1,13 @@
 package cn.bw.leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 /**
- * @description:
+ * @description: 145 二叉树的后序遍历
+ * @思路
  * @auther: bian.wu
  * @date: 2019/5/24 10:56
  */
@@ -20,24 +22,53 @@ public class A145二叉树的后序遍历 {
             val = x;
         }
     }
+    //非递归 解法
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> results = new LinkedList<>();
+        List<Integer> results = new ArrayList<>();
+        if(root==null){
+            return results;
+        }
         Stack<TreeNode> nodeStack = new Stack<>();
 
         nodeStack.push(root);
         while (!nodeStack.isEmpty()){
 
             root = nodeStack.pop();
+            //从前往后放 ， 先放root, 然后 root.right, 然后root.left;
+
             results.add(0,root.val);
             if(root.left!=null){
+                //stack 先压left,
                 nodeStack.push(root.left);
             }
             if(root.right !=null){
+                //stack 后压right，
                 nodeStack.push(root.right);
             }
 
         }
         return results;
-
     }
+
+    //递归解法
+    public List<Integer> postorderTraversal_two(TreeNode root){
+        List<Integer> results = new ArrayList<>();
+
+        if(root == null){
+            return results;
+        }
+        postorder(root,results);
+        return results;
+    }
+
+    private void postorder(TreeNode root,List<Integer> res){
+        if(root == null){
+            return;
+        }
+        postorder(root.left,res);
+        postorder(root.right,res);
+        res.add(root.val);
+    }
+
+
 }
