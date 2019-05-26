0531588d95d858c97a8ececfbd8f135971893bd8
Richard.Wu <richard.wu@RichardMac.lan>
2019-05-26 11:09:44 +0800

    git tracks from new macbook pro

---
+++ src/main/java/cn/bw/leetcode/practices/hellotest.java
@@ -0,0 +1,11 @@
+package cn.bw.leetcode.practices;
+
+/**
+ * this is a test from new macbook pro 13
+ */
+public class hellotest {
+
+    public static void main(String ...args){
+        System.out.println(" heelllooo  ..... ");
+    }
+}
