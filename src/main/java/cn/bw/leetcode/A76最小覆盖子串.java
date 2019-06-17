f2b9583cd6c777c54a6d9c3dea66cf8a21e5625c
Richard.Wu <120793495@qq.com>
2019-06-17 19:06:59 +0800

    prac daily

--- src/main/java/cn/bw/leetcode/A76最小覆盖子串.java
+++ src/main/java/cn/bw/leetcode/A76最小覆盖子串.java
@@ -1,5 +1,8 @@
 package cn.bw.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /***
  * 76最小覆盖子串
  *
@@ -21,6 +24,65 @@ package cn.bw.leetcode;
  * https://leetcode-cn.com/problems/minimum-window-substring/submissions/
  */
 public class A76最小覆盖子串 {
+
+    // 英文官方答案
+    public String minWindow0(String s, String t) {
+
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
+        }
+        //保存t中所有唯一字符计数的字典。
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
+        }
+
+        // t中的唯一字符数，需要出现在所需的窗口中。
+        int required = dictT.size();
+        // 左 右 指针
+        int l = 0, r = 0;
+    /*
+    formed 用于跟踪当前窗口中以其所需频率存在多少个唯一字符。
+    例如 如果t是“AABC”那么窗口必须有两个A，一个B和一个C.
+    因此，当满足所有这些条件时，formed = 3。
+    */
+        int formed = 0;
+        // 字典，用于保存当前窗口中所有唯一字符的计数。
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+        // (窗口长度, 左指针, 右指针)
+        int[] ans = {-1, 0, 0};
+        while (r < s.length()) {
+            // 从右侧向窗口添加一个字符
+            char c = s.charAt(r);
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+            //如果添加的当前字符的频率等于t中的所需计数，则将formed增加1。
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+            // 尝试并收缩窗口，直到它不再是“理想的”。
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+                // 更新满足条件的最小的窗口和 l r 指针
+                if (ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+                //Left”指针指向的位置处的字符不再是窗口的一部分。
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                // 将左指针向前移动，这将有助于查找新窗口。
+                l++;
+            }
+            // 继续扩大窗口
+            r++;
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
 
     public String minWindow(String s, String t) {
 
@@ -59,12 +121,12 @@ public class A76最小覆盖子串 {
     }
 
     /***
-     * @beat  95%
+     * @beat 95%
      * @param s
      * @param t
      * @return
      */
-    public String minWindwos(String s, String t) {
+    public String minWindows1(String s, String t) {
         // sChars 中 找 包含 pChars 的最小子串
 
         char[] sChars = s.toCharArray();
@@ -83,8 +145,10 @@ public class A76最小覆盖子串 {
             //减小计数
             if (pMap[sChars[j]] > 0)
                 count--;
+
             pMap[sChars[j]]--;
             j++;
+
             //计数为 0说明区间[i,j-1] 包含 p
             while (count == 0) {
                 //求得一个解
@@ -103,5 +167,53 @@ public class A76最小覆盖子串 {
         return minLen == s.length() + 1 ? "" : s.substring(l, r);
     }
 
+
+    /***
+     * another
+     * @param s
+     * @param count
+     */
+    private void fillEntries(String s, int[] count) {
+        for (char c : s.toCharArray())
+            count[c]++;
+    }
+
+    public String minWindow3(String s, String t) {
+        int[] need = new int[128];
+        fillEntries(t, need);
+
+        int i = 0, j = 0, l = 0, r = 0, missing = t.length();
+        while (r < s.length()) {
+            char right = s.charAt(r);
+            if (need[right] > 0) missing -= 1;
+            need[right]--;
+            r += 1;
+            while (missing == 0) {
+                if (j == 0 || (r - l) < (j - i)) {
+                    j = r;
+                    i = l;
+                }
+                char left = s.charAt(l);
+                need[left]++;
+                if (need[left] > 0) missing += 1;
+                l += 1;
+            }
+        }
+        return s.substring(i, j);
+    }
+
+    public static void main(String... args) {
+
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        A76最小覆盖子串 a = new A76最小覆盖子串();
+//        String result = a.minWindwos(s,t);
+
+        String result = a.minWindows1(s, t);
+
+        System.out.println(result);
+        System.out.println("end");
+    }
 
 }
