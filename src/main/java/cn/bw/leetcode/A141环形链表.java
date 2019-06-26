f30d0335bdd21c2ee815eb71b867873176857a81
Richard.Wu <120793495@qq.com>
2019-06-26 16:55:02 +0800

    141 环形链表

---
+++ src/main/java/cn/bw/leetcode/A141环形链表.java
@@ -0,0 +1,30 @@
+package cn.bw.leetcode;
+
+public class A141环形链表 {
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head){
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
